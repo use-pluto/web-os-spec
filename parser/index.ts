de038459f23d053d982883db484c8ff0f761b6f0
ip07 <78528552+its-pablo@users.noreply.github.com>
2022-09-16 14:53:43 -0700

    write the beginning of the parser

--- parser/index.ts
+++ parser/index.ts
@@ -1,1 +1,13 @@
+(function () {
+  let item = new URLSearchParams(location.search).get("protocol");
+  //if the user didn't come from web+os
+  if (item === null) {
+    return;
+  }
+  //regexes for understanding approaches
+  if (/^\/$/.test(item) === false) { //APP APPROACH
 
+  } else { //FILE APPROACH
+
+  }
+})();
