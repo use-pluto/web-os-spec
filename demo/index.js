fdcce5788d37517afe73719d2bc0da3e59d7a0d7
stretch07 <78528552+its-pablo@users.noreply.github.com>
2022-11-15 14:28:54 -0800

    fix demo again

--- demo/index.js
+++ demo/index.js
@@ -1,7 +1,17 @@
-navigator.registerProtocolHandler("web+os", "https://system41.github.io/web-os-standard/?protocol=%s", "web-os handler")
-let content = document.getElementById("content")
-content.innerHTML = `Tried to register protocol handler, visit a link such as <a href="web+os:directory/file.txt">this one</a> to see if it was successful`
-let theStuff = new URLSearchParams(location.href).get("protocol")
-if (theStuff) {
-    content.innerHTML = `Well done! You tried to open ${theStuff}` //yes, I know this is quite insecure (bc innerHTML) but it's just a quick demo
+function register() {
+  navigator.registerProtocolHandler(
+    "web+os",
+    "https://system41.github.io/web-os-standard/?protocol=%s",
+    "web-os handler"
+  );
+  content.innerHTML = `Tried to register protocol handler, visit a link such as <a href="web+os:directory/file.txt">this one</a> to see if it was successful`;
+  let theStuff = new URLSearchParams(location.href).get("protocol");
+  if (theStuff) {
+    content.innerHTML = `Well done! You tried to open ${theStuff}`; //yes, I know this is quite insecure (bc innerHTML) but it's just a quick demo
+  }
 }
+
+let content = document.getElementById("content");
+content.innerText =
+  "Registering a protocol handler requires user interaction... Click anywhere on the page to begin";
+document.body.onclick = register();
