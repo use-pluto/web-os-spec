b040dcc4b115cf9351c11361dc3d5740c274961a
stretch07 <78528552+its-pablo@users.noreply.github.com>
2022-11-29 15:44:57 -0800

    Update index.js

--- demo/index.js
+++ demo/index.js
@@ -4,7 +4,7 @@ function register() {
     "https://system41.github.io/web-os-standard/demo?protocol=%s",
     "web-os handler"
   );
-  content.innerHTML = `Tried to register protocol handler, visit a link such as <a href="web+os:directory/file.txt">this one</a> to see if it was successful`;
+  content.innerHTML = `Tried to register protocol handler, visit a link such as <a target="_blank" href="web+os:directory/file.txt">this one</a> to see if it was successful`;
   let theStuff = new URLSearchParams(location.href).get("protocol");
   if (theStuff) {
     content.innerHTML = `Well done! You tried to open ${theStuff}`; //yes, I know this is quite insecure (bc innerHTML) but it's just a quick demo
