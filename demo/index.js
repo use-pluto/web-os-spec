36eb1b0b2c324cc86d615fde9ad2d236699fcd02
stretch07 <78528552+its-pablo@users.noreply.github.com>
2022-11-30 12:10:06 -0800

    Update index.js

--- demo/index.js
+++ demo/index.js
@@ -5,7 +5,8 @@ function register() {
     "web-os handler"
   );
   content.innerHTML = `Tried to register protocol handler, visit a link such as <a target="_blank" href="web+os:directory/file.txt">this one</a> to see if it was successful`;
-  let theStuff = new URLSearchParams(location.href).get("protocol");
+  let url = new URL(location.href)
+  let theStuff = url.searchParams.get("protocol")
   if (theStuff) {
     content.innerHTML = `Well done! You tried to open ${theStuff}`; //yes, I know this is quite insecure (bc innerHTML) but it's just a quick demo
   }
